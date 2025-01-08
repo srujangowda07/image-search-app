ea17d1587385cd70ec609fcedbc1f1ee0bf443e2
srujangowda07 <srujan.24bcs10339@sst.scaler.com>
2025-01-08 15:56:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const accessKey = "GeoJO5Hbu1pCEM8ZIE3k9p7nnhpdjDYOeTD-4_mGOqo";
 
 async function searchImages() {
     let query = searchInput.value;
-    const url = "https://api.unsplash.com/search/photos?page=" + page + "&query=" + query + "&client_id=" + accessKey + "&per_page=9";
+    const url = "https://api.unsplash.com/search/photos?page=" + page + "&query=" + query + "&client_id=" + accessKey + "&per_page=12";
 
     const response = await fetch(url);
     const data = await response.json();
